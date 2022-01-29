b2a4a2b2a693d2720dbb524c23a5bc720cd0095b
austinjs <ajsprunger@gmail.com>
2022-01-29 15:16:46 -0700

    finishes game with first bonus

--- app.js
+++ app.js
@@ -3,8 +3,42 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  userGuess: null,
+  previousGuesses: [],
+  promptText: null,
+  getGuess: function(){
+    console.log(this.secretNum)
+    this.promptText = `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
+    this.userGuess = Number(window.prompt(this.promptText)) 
+    while (isNaN(this.userGuess) || this.userGuess < this.smallestNum || this.userGuess > this.biggestNum){
+      this.promptText = `Please enter a valid number`
+      this.userGuess = Number(window.prompt(this.promptText))
+    } 
+  },
+  render: function() {
+    if (this.userGuess === this.secretNum) {
+      alert(`Congrats! You guessed the number in ${this.previousGuesses.length}`)
+    } else if(this.userGuess > this.secretNum) {
+      alert(`Your guess is too high. Previous guesses: ${this.previousGuesses.join(', ')}`)
+    } else {
+      alert(`Your guess is too low. Previous guesses: ${this.previousGuesses.join(', ')}`)
+    }
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      while (this.userGuess !== this.secretNum){
+      this.getGuess()
+      if (this.userGuess < this.secretNum){
+        this.smallestNum = this.userGuess
+      } else {
+        this.biggestNum = this.userGuess
+      }
+      this.previousGuesses.push(this.userGuess)
+      this.render()
+    }
+    return
   }
 }
+
+game.play()
